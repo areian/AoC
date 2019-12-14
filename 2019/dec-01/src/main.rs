d1542f793e5a21f1acb817414abd05f4e52409e0
Rune T. Sorensen <dev@areian.net>
2019-12-14 18:12:07 +0100

    Dec-01 - Part 1

---
+++ 2019/dec-01/src/main.rs
@@ -0,0 +1,35 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+fn main() -> std::io::Result<()> {
+    let file = File::open("input")?;
+    let buf_reader = BufReader::new(file);
+
+    let mut total_fuel_required = 0;
+    for line in buf_reader.lines() {
+        let mass: isize = line?.parse().unwrap();
+        total_fuel_required += fuel_required(mass);
+    }
+
+    println!("Total fuel required: {}", total_fuel_required);
+
+    return Ok(());
+}
+
+fn fuel_required(mass: isize) -> isize {
+    return mass / 3 - 2;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fuel_required() {
+        assert_eq!(fuel_required(12), 2);
+        assert_eq!(fuel_required(14), 2);
+        assert_eq!(fuel_required(1969), 654);
+        assert_eq!(fuel_required(100756), 33583);
+    }
+}
