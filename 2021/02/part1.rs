46c567091bf18f291705a52bea86b51aba3d3c3e
Rune T. Sorensen <dev@areian.net>
2021-12-09 21:15:22 +0000

    Initial attempt at 2021-12-02 part 1

---
+++ 2021/02/part1.rs
@@ -0,0 +1,21 @@
+use std::fs;
+
+fn parse_command(command: &str) -> (isize, isize) {
+    let cmd: Vec<&str> = command.split_whitespace().collect();
+    match cmd[0] {
+        "up" => (-1 * cmd[1].parse::<isize>().expect("NaN"), 0),
+        "down" => (cmd[1].parse::<isize>().expect("NaN"), 0),
+        "forward" => (0, cmd[1].parse::<isize>().expect("NaN")),
+        _ => panic!("Unknown command"),
+    }
+}
+
+fn main() {
+    let input = fs::read_to_string("input").expect("Something went wrong reading the file");
+    let inputs = input
+        .lines()
+        .map(parse_command)
+        .reduce(|acc, x| (acc.0 + x.0, acc.1 + x.1))
+        .unwrap();
+    println!("{}", inputs.0 * inputs.1);
+}
