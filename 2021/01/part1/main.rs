b018ce9dde778ba1f0e8e0bcccae54b9a5697edf
Rune T. Sorensen <dev@areian.net>
2021-12-12 22:51:47 +0000

    Update 01-part1 to use map/fold for better readability

--- 2021/01/part1/main.rs
+++ 2021/01/part1/main.rs
@@ -1,28 +1,16 @@
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
+use std::fs;
 
 fn main() {
-    let f = File::open("../input").expect("File not found");
-    let mut reader = BufReader::new(f);
-    let mut line = String::new();
-
-    reader.read_line(&mut line).expect("Could not read line");
-
-    print!("{}", line);
-    let mut last = line.trim_end().parse::<i32>().expect("Not a number");
-    line.clear();
-    let mut incr = 0;
-
-    while reader.read_line(&mut line).expect("Could not read line") > 0 {
-      print!("{}", line);
-      let i = line.trim_end().parse::<i32>().expect("Not a number");
-      if i > last {
-        incr = incr + 1;
-      }
-      last = i;
-      line.clear();
-    }
-
-    println!("Increases: {}", incr);
+    let input = fs::read_to_string("../input").expect("Something went wrong reading the file");
+    let (incr, _) = input
+        .lines()
+        .map(|line| line.parse::<i32>().expect("Not a number"))
+        .fold((0, 0), |(incr, last), i| {
+            if i > last {
+                (incr + 1, i)
+            } else {
+                (incr, i)
+            }
+        });
+    println!("{}", incr - 1); // don't count increase on first line
 }
